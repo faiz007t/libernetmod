dd340d822d00001cf696f1ae39061d7c5b4e49a6
faiz007t <faiziz18@gmail.com>
2025-05-10 22:40:10 +0800

    Update navbar.php

--- navbar.php
+++ navbar.php
@@ -2,6 +2,16 @@
     $url_array =  explode('/', $_SERVER['REQUEST_URI']) ;
     $url = end($url_array);
 ?>
+<!-- Add this style block in your <head> or in your CSS file -->
+<style>
+.navbar-toggler {
+    border-color: rgba(255,255,255,0.5);
+}
+.navbar-toggler-icon {
+    background-image: url("data:image/svg+xml;charset=utf8,%3Csvg viewBox='0 0 30 30' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath stroke='rgba%28255,255,255,1%29' stroke-width='2' stroke-linecap='round' stroke-miterlimit='10' d='M4 7h22M4 15h22M4 23h22'/%3E%3C/svg%3E");
+}
+</style>
+
 <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #11195b;">
     <a class="navbar-brand" href="#"></a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
