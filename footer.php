f28627ed44617b63154c5a7fae72f900020a4555
faiz007t <faiziz18@gmail.com>
2025-05-11 15:37:21 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,3 +1,3 @@
 <footer class="text-center">
-    <font color="white">© 2025 <a href="https://github.com/faiz007t/libernet">Libernet Mod</a> v1.5.4. All rights reserved. Modified by </font><a href="https://t.me/faisal971">faisal971</a>
+    <font color="white">© 2025 <a href="https://github.com/faiz007t/libernetmod">Libernet Mod</a> v1.5.4. All rights reserved. | Modified by </font><a href="https://t.me/faisal971">faisal971</a>
 </footer>
