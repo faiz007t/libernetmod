a3751cb6c366b8010b7e3480aaddaa7203cc6235
faiz007t <faiziz18@gmail.com>
2025-05-12 16:09:56 +0800

    Update index.php

--- index.php
+++ index.php
@@ -115,7 +115,7 @@
                                         <div class="form-check">
                                             <input class="form-check-input" type="checkbox" v-model="config.system.tunnel.dns_resolver" :disabled="status === true" id="dns-resolver">
                                             <label class="form-check-label" for="dns-resolver">
-                                                <i class="bi bi-server"></i> DNS resolver
+                                                <i class="fas fa-network-wired"></i> DNS resolver
                                             </label>
                                         </div>
                                     </div>
