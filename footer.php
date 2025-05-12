dda7d4719e24f75e971ab83cb25bf5db3595b558
faiz007t <faiziz18@gmail.com>
2025-05-12 20:05:25 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 <footer class="text-center">
-    <span style="color: dark-grey;">
+    <span style="color: grey;">
     © 2025 
     <a href="https://github.com/faiz007t/libernetmod" rel="noopener noreferrer">
       Libernet Mod
